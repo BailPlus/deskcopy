cc5a80b696c5145e56620046333e84c4a755f7a3
Bail(徐中机房) <2915289604@qq.com>
2023-04-18 10:34:26 +0800

    优化代码结构

--- deskcopy.py
+++ deskcopy.py
@@ -1,22 +1,21 @@
-#Copyright Bail 2022
-#deskcopy 桌面拖入文件自动复制 v1.0_1
-#2022.11.18-2022.12.10
+#Copyright Bail 2022-2023
+#deskcopy 桌面拖入文件自动复制 v1.8.5_36
+#2022.11.18-2023.4.18
 
 TARGET = 'D:\\desktop'  #复制目标
-ALLOW_SUFFIX = ('.doc','.docx','.ppt','.pptx','.pdf')   #过滤后缀名时允许的类型
 LOGFILE = 'D:\\deskcopy.log'    #日志文件
 UPANPATH = 'E:\\'
+ALLOW_SUFFIX = ('.doc','.docx','.ppt','.pptx','.pdf')   #过滤后缀名时允许的类型
 UPACOPY_ARGV = '--upan' #U盘全盘复制启动参数
+STRFTIME = '%Y.%m.%d %H:%M:%S'  #格式化时间格式
 DESKSLEEP = 5   #桌面复制间隔时间
 UPANSLEEP = 5   #U盘检测间隔时间
 KILL360SLEEP = 60   #杀死360画报间隔时间
-STRFTIME = '%Y.%m.%d %H:%M:%S'  #格式化时间格式
 UPLOADSLEEP = 60    #上传课件间隔时间
 
 import os,time,shutil,sys,threading
 
 os.chdir(os.path.join(os.path.expanduser('~'),'Desktop'))
-filesizes = []
 
 def execute_with_arg():
     '''使用参数启动'''
@@ -26,9 +25,8 @@ def execute_with_arg():
             sys.exit()
         else:
             opencopy(sys.argv[1])
-
             sys.exit()
-def upancopy(): #※不可在Linux下测试
+def upancopy():
     '''U盘全盘复制'''
     while not os.path.exists(UPANPATH):
         time.sleep(UPANSLEEP)
@@ -63,11 +61,11 @@ target(str):目标路径
 isfilter(bool):是否过滤后缀名'''
     filelst = os.walk(path)
     for i in filelst:
-        for j in i[1]:
+        for j in i[1]:  #创建文件夹
             dirname = os.path.join(target,i[0],j)
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
-        for j in i[2]:
+        for j in i[2]:  #复制文件
             filesrc = os.path.join(i[0],j)
             suffix = os.path.splitext(filesrc)[-1]
             if (suffix in ALLOW_SUFFIX) or (not isfilter):
@@ -95,7 +93,7 @@ logtext(str):日志内容'''
         print(content,file=logfile)
 def deskcopy():
     '''桌面复制'''
-    global filesizes
+    filesizes = []
     for i in os.listdir():
         filesizes.append(os.stat(i).st_size)
     while True:
@@ -111,9 +109,10 @@ def main():
     log('I','已启动')
     threading.Thread(target=kill360).start()
     deskcopy()
+    return 0
 
 if __name__ == '__main__':
     try:
-        main()
+        sys.exit(main())
     except Exception as e:
         log('F',e)
